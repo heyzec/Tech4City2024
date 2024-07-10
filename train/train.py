2378b2f94fe74c18856016639036e15966e086bd
ruihan00 <ruixhan@gmail.com>
2024-07-11 00:24:16 +0800

    add requirmeents

--- train/train.py
+++ train/train.py
@@ -4,6 +4,6 @@ dataset_yaml = './datasets/dataset.yaml'
 
 model = YOLO('yolov8n.pt') 
 # modify device to others if you are not using arm mac lol
-model.train(data=dataset_yaml, epochs=1)
+model.train(data=dataset_yaml, epochs=5)
 
 model.save('yolov8_custom.pt')
