3cfa88af413b5041300c644eb3d0dcc543e57bd3
yeoyujie <yeo.yujie@gmail.com>
2024-07-14 01:33:10 +0800

    Merge stuffs

---
+++ train/train.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+
+dataset_yaml = './datasets/dataset.yaml'
+
+model = YOLO('yolov8n.pt') 
+# modify device to others if you are not using arm mac lol
+model.train(data=dataset_yaml, epochs=5)
+
+model.save('yolov8_custom.pt')
